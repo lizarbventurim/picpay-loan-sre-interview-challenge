d9a0bad2c3795fc0eec4b42c4551a48350eb4b18
ademir.silva <ademir.silva@picpay.com>
2024-11-26 10:00:32 -0300

    chore: remove picpay mentions

--- services/api/src/main/java/com/sre/challenge/Application.java
+++ services/api/src/main/java/com/sre/challenge/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.picpay")
+@EntityScan(basePackages = "com.sre")
 public class Application {
 
 	public static void main(String[] args) {
