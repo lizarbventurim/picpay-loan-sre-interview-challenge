4ff182a5f6dc5af1ecc95e0e8fcd78c74303aa73
lizarbventurim <lizarbpacheco@gmail.com>
2025-01-17 18:36:47 -0300

    feat: set configuration cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://meus-emprestimos.picpay.com", // insere baseurl para ser utilizado em todos os testes
+    video: true, // habilita gravação de vídeo
+    defaultBrowser: "electron", // define o navegador padrão ao rodar o npn run test e ao abrir o cypress já abre por padrão o electron
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
 });
