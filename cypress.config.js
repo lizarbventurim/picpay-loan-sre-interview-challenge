e33946d481a20dfd2ccb5619200ad1dec23eed61
lizarbventurim <lizarbpacheco@gmail.com>
2025-01-17 18:45:19 -0300

    feat: set configuration resolution

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     baseUrl: "https://meus-emprestimos.picpay.com", // insere baseurl para ser utilizado em todos os testes
     video: true, // habilita gravação de vídeo
     defaultBrowser: "electron", // define o navegador padrão ao rodar o npn run test e ao abrir o cypress já abre por padrão o electron
+    viewportWidth: 1280, // seta a largura da tela
+    viewportHeight: 720,  // seta a altura da tela
     setupNodeEvents(on, config) {
       
     },
