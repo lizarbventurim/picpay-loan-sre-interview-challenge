7485619546543092bc3251087fc0b80c8b7fa021
lizarbventurim <lizarbpacheco@gmail.com>
2025-01-18 10:24:13 -0300

    feat: configure eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import cypress from "eslint-plugin-cypress";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/node_modules/", "cypress/videos/", "cypress/screenshots/"],
+}, ...compat.extends("eslint:recommended", "plugin:cypress/recommended"), {
+    plugins: {
+        cypress,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+            ...cypress.environments.globals.globals,
+        },
+    },
+
+    rules: {
+        "no-unused-vars": "warn",
+        "no-console": "off",
+        "cypress/no-unnecessary-waiting": "warn",
+    },
+}];
