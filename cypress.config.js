fc9d1f4d1f8ccd0724c0bb0b85684ecef90b36da
lizarbventurim <lizarbpacheco@gmail.com>
2025-01-18 10:38:31 -0300

    feat: format documents

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://meus-emprestimos.picpay.com", // insere baseurl para ser utilizado em todos os testes
+    baseUrl: 'https://meus-emprestimos.picpay.com', // insere baseurl para ser utilizado em todos os testes
     video: true, // habilita gravação de vídeo
-    defaultBrowser: "electron", // define o navegador padrão ao rodar o npn run test e ao abrir o cypress já abre por padrão o electron
+    defaultBrowser: 'electron', // define o navegador padrão ao rodar o npn run test e ao abrir o cypress já abre por padrão o electron
     viewportWidth: 1280, // seta a largura da tela
-    viewportHeight: 720,  // seta a altura da tela
-    setupNodeEvents(on, config) {
-      
-    },
+    viewportHeight: 720, // seta a altura da tela
+    setupNodeEvents(on, config) {},
   },
-});
+})
